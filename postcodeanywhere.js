c52985ee56ec5c829422584bc23b248ccce2e9d1
kenorb <kenorb@gmail.com>
2015-12-14 10:59:15 +0000

    Drupal.jsAC.prototype.populatePopup by David: Show popup with matches.

--- postcodeanywhere.js
+++ postcodeanywhere.js
@@ -176,7 +176,82 @@
               });
             } // end: else
 
-          } // end: Drupal.jsAC
+
+
+          };// end: Drupal.jsAC
+
+          /**
+           * Positions the suggestions popup and starts a search.
+           */
+          Drupal.jsAC.prototype.populatePopup = function () {
+            var $input = $(this.input);
+            // Show popup.
+            if (this.popup) {
+              $(this.popup).remove();
+            }
+            this.selected = false;
+            this.popup = $('<div class="dropdown"></div>')[0];
+            this.popup.owner = this;
+            $input.parent().after(this.popup);
+
+            // Do search.
+            this.db.owner = this;
+            this.db.search(this.input.value);
+          };
+
+          /**
+           * Fills the suggestion popup with any matches received.
+           */
+          Drupal.jsAC.prototype.found = function (matches) {
+            // If no value in the textfield, do not show the popup.
+            if (!this.input.value.length) {
+              return false;
+            }
+
+            // Prepare matches.
+            var ul = $('<ul class="dropdown-menu"></ul>');
+            var ac = this;
+            ul.css({
+              display: 'block',
+              right: 0
+            });
+            for (var key in matches) {
+              if (key == 'number-results') {
+                $('.js-postcodeanywhere-number-results').remove();
+                var numberOfResults = $('<div class="form__input-sub-info js-postcodeanywhere-number-results"></div>').html(matches[key]);
+                $('div.form-type-postcodeanywhere-autocomplete').append(numberOfResults);
+              } else {
+                var listElement = '';
+                $('<li></li>')
+                    .html($('<a class="' + key + '" href="#"></a>').html(matches[key]).click(function (e) {
+                      e.preventDefault();
+                    }))
+                    .mousedown(function () {
+                      ac.select(this);
+                    })
+                    .mouseover(function () {
+                      ac.highlight(this);
+                    })
+                    .mouseout(function () {
+                      ac.unhighlight(this);
+                    })
+                    .data('autocompleteValue', key)
+                    .appendTo(ul);
+              }
+            }
+
+            // Show popup with matches, if any.
+            if (this.popup) {
+              if (ul.children().length) {
+                $(this.popup).empty().append(ul).show();
+                $(this.ariaLive).html(Drupal.t('Autocomplete popup'));
+              }
+              else {
+                $(this.popup).css({ visibility: 'hidden' });
+                this.hidePopup();
+              }
+            }
+          };
         }
 
         // Add onclick event to lookup button.
